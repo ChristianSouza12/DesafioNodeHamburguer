34b4a04bcd2d9eab07e5de7ae9cd2cdcf63413ed
ChristianSouza12 <christianlmsouza@outlook.com>
2024-03-14 08:30:02 -0300

    Alteração rotas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const uuid = require('uuid');
 const app = express();
 const cors = require('cors');
